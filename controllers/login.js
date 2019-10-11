cc1fd7b570e902ec7e9492d0ff58cd80ee1f572b
Declan Soper (soperd) <soperd@coventry.ac.uk>
2019-10-11 01:52:45 +0100

    add login functionality

--- controllers/login.js
+++ controllers/login.js
@@ -1,15 +1,29 @@
 
 const Router = require('koa-router')
+const koaBody = require('koa-body')
+const bcrypt = require('bcrypt')
 
 const login = new Router({ prefix: '/login' })
 
+login.use(koaBody())
+
 login.get('/', async ctx => {
-  await ctx.render('login.hbs')
+  return ctx.render('login.hbs')
 })
 
 login.post('/', async ctx => {
-  console.log(ctx.query)
-  console.log(ctx.querystring)
+  const { username, password } = ctx.request.body
+
+  const user = await ctx.db.getUser(username)
+  if (!user) {
+    return ctx.render('login.hbs', { errorMsg: 'User does not exist' })
+  }
+
+  if (await bcrypt.compare(password, user.hash)) {
+    return ctx.redirect('/')
+  } else {
+    return ctx.render('login.hbs', { errorMsg: 'Password incorrect' })
+  }
 })
 
 module.exports = login
