3d7fe1931f8432d4af0d835ee07281131c7714b6
Declan Soper (soperd) <soperd@coventry.ac.uk>
2019-11-11 19:24:47 +0000

    Tidy and add missing 'use strict'

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const Router = require('koa-router')
-const signup = new Router() 
+const signup = new Router()
 
 
 signup.get('/signup', async ctx => {
 	await ctx.render('signup.hbs')
 })
 
-signup.post('/signup', async ctx =>{
+signup.post('/signup', async ctx => {
 	console.log(ctx.query)
 	console.log(ctx.querystring)
 })
