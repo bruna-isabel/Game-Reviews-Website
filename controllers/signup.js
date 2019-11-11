85a73fa0dc09d9abee449055bd19908b1bd53003
ricardob <ricardob@coventry.ac.uk>
2019-11-11 13:09:08 +0000

    Koa endpoint for the sign up page with sign up.hbs file just for testing

---
+++ controllers/signup.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Router = require('koa-router')
+const signup = new Router() 
+
+
+signup.get('/signup', async ctx => {
+	await ctx.render('signup.hbs')
+})
+
+signup.post('/signup', async ctx =>{
+	console.log(ctx.query)
+	console.log(ctx.querystring)
+})
+
+module.exports = signup
+
