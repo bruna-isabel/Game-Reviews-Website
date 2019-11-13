534cc446b305a14c43c2a80d5229517a9ddb30ab
drumevp <drumevp@coventry.ac.uk>
2019-11-13 22:01:58 +0000

    Removed mock data from build_db.sql file
    Forgot to remove it in my last commit

--- build/build_db.sql
+++ build/build_db.sql
@@ -15,13 +15,3 @@ CREATE TABLE `games`
     `submittedBy` TEXT,
     `approved` TEXT
 );
-
-INSERT INTO `games` VALUES(1, 'game1', 'summary', 'url', 5, 'matthew', 'yes');
-INSERT INTO `games` VALUES(2, 'game2', 'summary', 'url', 5, 'matthew', 'no');
-INSERT INTO `games` VALUES(3, 'game3', 'summary', 'url', 5, 'matthew', 'no');
-INSERT INTO `games` VALUES(4, 'game4', 'summary', 'url', 5, 'matthew', 'yes');
-INSERT INTO `games` VALUES(5, 'game5', 'summary', 'url', 5, 'matthew', 'no');
-INSERT INTO `games` VALUES(6, 'game6', 'summary', 'url', 5, 'matthew', 'yes');
-INSERT INTO `games` VALUES(7, 'game7', 'summary', 'url', 5, 'matthew', 'no');
-INSERT INTO `games` VALUES(8, 'game8', 'summary', 'url', 5, 'matthew', 'yes');
-INSERT INTO `games` VALUES(9, 'game9', 'summary', 'url', 5, 'matthew', 'no');
