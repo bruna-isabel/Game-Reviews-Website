47b244b24a222bb67c74993ef75859aac4db5ac2
drumevp <drumevp@coventry.ac.uk>
2019-11-20 18:53:29 +0000

    Added redirect route from list to IGP
    Clicking on a game title in the list now redirects to its individual game page (router not yet implemented)
    
    Created hbs file to show errors

--- controllers/approval.js
+++ controllers/approval.js
@@ -8,59 +8,74 @@ const approval = new Router({prefix: '/approval'})
 
 approval.get('/games', async ctx => {
 	//maybe check if user is an admin
-	const games = await ctx.db.getGames()
-	let i
-	const unapproved = []
-	for (i = 0; i < games.length; i++) {
-		if(games[i]['approved'] !== 'yes') {
-			unapproved.push(games[i])
-		} else {
-			continue
+	try {
+		const games = await ctx.db.getGames()
+		let i
+		const unapproved = []
+		for (i = 0; i < games.length; i++) {
+			if(games[i]['approved'] !== 'yes') {
+				unapproved.push(games[i])
+			} else {
+				continue
+			}
 		}
+		await ctx.render('approvalGames.hbs', {games: unapproved})
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
 	}
-	//or just run a query to get all where approved == 'no'
-	await ctx.render('approvalGames.hbs', {games: unapproved})
 })
 
 approval.post('/games', koaBody, async ctx => {
-	const body = ctx.request.body
-	const id = parseInt(Object.keys(body)[0]) //getting the key (gameID) and converting it into an integer
-	const game = await ctx.db.getGame(id)
-	if(body[id] === 'Approve') {
-		game['approved'] = 'yes'
-		await ctx.db.updateGame(game)
-	} else if(body[id] === 'Reject') {
-		await ctx.db.deleteGame(id)
+	try {
+		const body = ctx.request.body
+		const id = parseInt(Object.keys(body)[0]) //getting the key (gameID) and converting it into an integer
+		const game = await ctx.db.getGame(id)
+		if(body[id] === 'Approve') {
+			game['approved'] = 'yes'
+			await ctx.db.updateGame(game)
+		} else if(body[id] === 'Reject') {
+			await ctx.db.deleteGame(id)
+		}
+		ctx.redirect('/approval/games')
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
 	}
-	ctx.redirect('/approval/games')
 })
 
 approval.get('/reviews', async ctx => {
-	const reviews = await ctx.db.getReviews()
-	let i
-	const unapproved = []
-	for (i = 0; i < reviews.length; i++) {
-		if(reviews[i]['approved'] !== 'yes') {
-			unapproved.push(reviews[i])
-		} else {
-			continue
+	try {
+		const reviews = await ctx.db.getReviews()
+		let i
+		const unapproved = []
+		for (i = 0; i < reviews.length; i++) {
+			if(reviews[i]['approved'] !== 'yes') {
+				unapproved.push(reviews[i])
+			} else {
+				continue
+			}
 		}
+		await ctx.render('approvalReviews', {review: unapproved})
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
 	}
-	await ctx.render('approvalReviews', {review: unapproved})
 })
 
 approval.post('/reviews', koaBody, async ctx => {
-	const body = ctx.request.body
-	const id = parseInt(Object.keys(body)[0])
-	const review = await ctx.db.getReview(id)
-	console.log(review)
-	if(body[id] === 'Approve') {
-		review['approved'] = 'yes'
-		await ctx.db.updateReview(review)
-	} else if(body[id] === 'Reject') {
-		await ctx.db.deleteReview(id)
+	try {
+		const body = ctx.request.body
+		const id = parseInt(Object.keys(body)[0])
+		const review = await ctx.db.getReview(id)
+		console.log(review)
+		if(body[id] === 'Approve') {
+			review['approved'] = 'yes'
+			await ctx.db.updateReview(review)
+		} else if(body[id] === 'Reject') {
+			await ctx.db.deleteReview(id)
+		}
+		await ctx.redirect('/approval/reviews')
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
 	}
-	await ctx.redirect('/approval/reviews')
 })
 
 module.exports = approval
--- controllers/list.js
+++ controllers/list.js
@@ -5,18 +5,21 @@ const Router = require('koa-router')
 const list = new Router({prefix: '/list'})
 
 list.get('/', async ctx => {
-	const games = await ctx.db.getGames()
-	let i
-	const approved = []
-	for (i = 0; i < games.length; i++) {
-		if(games[i]['approved'] === 'yes') {
-			approved.push(games[i])
-		} else {
-			continue
+	try {
+		const games = await ctx.db.getGames()
+		let i
+		const approved = []
+		for (i = 0; i < games.length; i++) {
+			if(games[i]['approved'] === 'yes') {
+				approved.push(games[i])
+			} else {
+				continue
+			}
 		}
+		await ctx.render('listpage.hbs', {games: approved})
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
 	}
-	console.log(approved)
-	await ctx.render('listpage.hbs', {games: approved})
 })
 
 list.post('/', async ctx => {
