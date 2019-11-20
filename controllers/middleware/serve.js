159ab3938542937dc3be9de8183b874ea3804d5c
Declan Soper <soperd@coventry.ac.uk>
2019-11-20 19:46:14 +0000

    Add static routing middleware
    Needed to create a custom middleware because koa-static doesn't let
    you customise the base URL for serving static files. This means
    that it has to live at /, when we would ideally like home to live
    there.
    
    Added the serve middleware in ./controllers/middleware/serve.js and
    a mimetype detection function in ./utils/mime.js

---
+++ controllers/middleware/serve.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const { getMimeType } = require('../../utils/mime')
+
+/**
+ * Options for serve.
+ * @typedef ServeOpts
+ * @type {object}
+ * @property {string} base - Base URL of the static content
+ * @property {string} folder - Folder of the static content being served
+ */
+
+/**
+ * Koa middleware for serving static content.
+ * @param {ServeOpts} opts
+ * @returns {function} Returns a Koa middleware function
+ */
+function serve(opts) {
+	opts.base = opts.base || '/'
+	opts.folder = opts.folder || './static'
+
+	return (ctx, next) => {
+		const basePos = ctx.request.url.indexOf(opts.base)
+		if (basePos >= 0) {
+			let filename = ctx.request.url.slice(basePos + opts.base.length)
+
+			// default to index.html
+			if (!filename) {
+				filename = 'index.html'
+			}
+
+			serveFile(ctx, path.join(opts.folder, filename))
+		}
+
+		next()
+	}
+}
+
+/**
+ * Serves a file via Koa.
+ * @param {*} ctx - Koa context object
+ * @param {string} filename - filename of the file being served
+ */
+function serveFile(ctx, filename) {
+	ctx.response.header.accepts = getMimeType(filename)
+	ctx.response.body = fs.readFileSync(filename, 'utf8')
+}
+
+module.exports = serve
