1bbb8ac7f055f63ec12360e7c761df8370cb706c
Callum Byrne <callumbyrne@Callums-MacBook-Pro.local>
2019-11-28 00:35:52 +0000

    merged with latest commit

--- controllers/game.js
+++ controllers/game.js
@@ -8,6 +8,7 @@ const Review = require('../models/review')
 // eslint-disable-next-line max-lines-per-function
 game.get('/game:id', async ctx => {
 	try {
+<<<<<<< HEAD
 		const gameID = Number(ctx.params.id)
 		if (isNaN(gameID)) {
 			throw new Error('game ID must be a number')
@@ -17,22 +18,31 @@ game.get('/game:id', async ctx => {
 		const reviewCount = reviews.length;
 		const platformsIds = gamedata.platforms.split(',');
 		const platforms = await ctx.db.getPlatforms(platformsIds);
+=======
+		const gamedata = await ctx.db.getGame(Number(ctx.params.id))
+		const reviews = (await ctx.db.getReviewsForGame(Number(gamedata.gameID))).reverse()
+		const reviewCount = reviews.length
+		const platformsIds = gamedata.platforms.split(',')
+		const platforms = await ctx.db.getPlatforms(platformsIds)
+>>>>>>> bb50ac1622da9c96b67122db7787d25b9db67a5e
 		const user = await ctx.db.getUser(ctx.session.userID)
-		let avgScore = 0;
+		let avgScore = 0
 		if (reviewCount > 0) {
-			avgScore = await ctx.db.getAvgScore(gamedata.gameID);
-		}
-		else {
-			avgScore = 0;
+			avgScore = await ctx.db.getAvgScore(gamedata.gameID)
+		} else {
+			avgScore = 0
 		}
 		if(gamedata.poster.startsWith('http')) {
 			gamedata.url = true
 		}
-		if (gamedata.approved === 'yes' || user.isAdmin === 'yes') { //so the game page can be opened by an admin during the approval process
-			await ctx.render('game', {review: reviews.slice(0,3),  expandedReview: reviews, thisgame: gamedata, reviewNo: reviewCount, platforms: platforms, avgScore: avgScore, user: ctx.session.authorised, admin: await ctx.db.isUserAdmin(ctx.session.userID)})
-		}
-		else {
-			ctx.redirect(`/list`)
+		if (gamedata.approved === 'yes' || user.isAdmin === 'yes') {
+		//so the game page can be opened by an admin during the approval process
+			const sliceInt = 0.3
+			await ctx.render('game', {review: reviews.slice(sliceInt), expandedReview: reviews, thisgame: gamedata,
+				reviewNo: reviewCount, platforms: platforms, avgScore: avgScore, user: ctx.session.authorised,
+				admin: await ctx.db.isUserAdmin(ctx.session.userID)})
+		} else {
+			ctx.redirect('/list')
 		}
 	} catch(err) {
 		
@@ -43,18 +53,16 @@ game.get('/game:id', async ctx => {
 game.get('/allReviews:id', async ctx => {
 	try {
 		const gamedata = await ctx.db.getGame(Number(ctx.params.id))
-		const reviews = (await ctx.db.getReviewsForGame(Number(gamedata.gameID))).reverse();
-		const reviewCount = reviews.length;
-		let avgScore = 0;
-		if (reviewCount > 0)
-		{
-			avgScore = await ctx.db.getAvgScore(gamedata.gameID);
+		const reviews = (await ctx.db.getReviewsForGame(Number(gamedata.gameID))).reverse()
+		const reviewCount = reviews.length
+		let avgScore = 0
+		if (reviewCount > 0) {
+			avgScore = await ctx.db.getAvgScore(gamedata.gameID)
+		} else {
+			avgScore = 0
 		}
-		else
-		{
-			avgScore = 0;
-		}
-		await ctx.render('allReviews', {review: reviews, thisgame: gamedata, reviewNo: reviewCount, avgScore: avgScore, user: ctx.session.authorised, admin: await ctx.db.isUserAdmin(ctx.session.userID)})
+		await ctx.render('allReviews', {review: reviews, thisgame: gamedata, reviewNo: reviewCount,
+			avgScore: avgScore, user: ctx.session.authorised, admin: await ctx.db.isUserAdmin(ctx.session.userID)})
 	} catch(err) {
 		ctx.body = err.message
 	}
@@ -63,10 +71,10 @@ game.get('/allReviews:id', async ctx => {
 game.post('/add', async ctx => {
 	try {
 		const body = ctx.request.body
-		let reviewText = body.rvtext;
+		const reviewText = body.rvtext
 		const user = await ctx.db.getUser(ctx.session.userID)
-		const review = new Review(user.username, body.gameID, body.starRating, reviewText, "DD/MM/YYYY", "no");
-		await ctx.db.createReview(review);
+		const review = new Review(user.username, body.gameID, body.starRating, reviewText, 'DD/MM/YYYY', 'no')
+		await ctx.db.createReview(review)
 		ctx.redirect(`/game${body.gameID}`)
 	} catch(err) {
 		ctx.redirect(`/?msg=ERROR: ${err.message}`)
