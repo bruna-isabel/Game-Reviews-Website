b5a9ee0ab0be1e836f4e55c2fd28df0e11b810e2
Callum Byrne <byrnec5@coventry.ac.uk>
2019-12-02 19:14:23 +0000

    Minor fixes to visuals and CSS tweaks
    Fixed appearance of the homepage so the elements are centred. Changed the visual appearance of the 'adding games' page.

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -4,6 +4,10 @@ const Router = require('koa-router')
 
 const homepage = new Router({ prefix: '/homepage' })
 
+homepage.get('', async ctx => {
+	await ctx.render('homepage.hbs',{user: ctx.session.authorised, admin: await ctx.db.isUserAdmin(ctx.session.userID)})
+})
+
 homepage.get('/', async ctx => {
 	await ctx.render('homepage.hbs',{user: ctx.session.authorised, admin: await ctx.db.isUserAdmin(ctx.session.userID)})
 })
